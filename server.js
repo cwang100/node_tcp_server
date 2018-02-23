a8393961f6b34d173e8030304eea0eece1918cea
unknown <cwang100@illinois.edu>
2018-02-23 18:08:41 +0800

    add room

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ var net = require('net');
 var server = net.createServer();  
   
 //聚合所有客户端  
-var sockets = [];  
-var randomSeeds = [];
-var readyCount = 0;
+var rooms = [];
+var socketRoomPair = {};
+var roomSize = 3;
 
 class TcpPack {
 	constructor(type, data) {
@@ -14,40 +14,22 @@ class TcpPack {
     this.data = data;
   }
 }
+
+class Room {
+	constructor() {
+		this.sockets = [];
+		this.randomSeeds = [];
+		this.readyCount = 0;
+		this.gameStarted = false;
+	}
+}
   
 //接受新的客户端连接  
 server.on('connection', function(socket) {  
   console.log('got a new connection');  
-  if (sockets.length > 4)
-  	return;
+  socket.uniqueId = _.uniqueId('socket_');
 
-  var encodedData = encode("connectSuccessful", "");
-
-	socket.write(encodedData); 
-
-  sockets.push(socket);  
-  //从连接中读取数据  
-  socket.on('data', function(data) {  
-		decodedDatas = decode(data); 
-    _.each(decodedDatas, function(decodedData) {
-    	handleReceivedData(socket, decodedData.type, decodedData.data);
-    }); 
-  });  
-
-  //删除被关闭的连接  
-  socket.on('close', function(){  
-    console.log('connection closed');  
-    var index = sockets.indexOf(socket);  
-    sockets.splice(index, 1);  
-    broadCastDisconnect(index);
-  }); 
-
-  socket.on('error', function(err) {
-    console.log('connection error:', err.message);  
-    var index = sockets.indexOf(socket);  
-    sockets.splice(index, 1); 
-    broadCastDisconnect(index);
-  });
+  setupSocket(socket);
 });  
   
 server.on('error', function(err) {  
@@ -63,6 +45,56 @@ server.listen(3000, function() {
 	randomSeed = date.getTime();
 }); 
 
+function addSocketToRoom(socket) {
+	for(var i = 0; i < rooms.length; i ++) {
+		if (rooms[i].sockets.length < roomSize && !rooms[i].gameStarted) {
+			rooms[i].sockets.push(socket);
+			socketRoomPair[socket.uniqueId] = i;
+			return;
+		}
+	}
+
+	createNewRoom(socket);
+}
+
+function createNewRoom(socket) {
+	var room = new Room();
+	room.sockets.push(socket);
+	rooms.push(room);
+	socketRoomPair[socket.uniqueId] = rooms.indexOf(room);
+}
+
+function setupSocket(socket) {
+  var encodedData = encode("connectSuccessful", "");
+
+	socket.write(encodedData); 
+
+  //从连接中读取数据  
+  socket.on('data', function(data) {  
+		decodedDatas = decode(data); 
+    _.each(decodedDatas, function(decodedData) {
+    	handleReceivedData(socket, decodedData.type, decodedData.data);
+    }); 
+  });  
+
+  //删除被关闭的连接  
+  socket.on('close', function() {  
+    console.log('connection closed');  
+    removeFromRoom(socket);
+  }); 
+
+  socket.on('error', function(err) {
+    console.log('connection error:', err.message);  
+    removeFromRoom(socket);
+  });
+}
+
+function removeFromRoom(socket) {
+	var roomIndex = socketRoomPair[socket.uniqueId];
+  var index = rooms[roomIndex].sockets.indexOf(socket);  
+  _.remove(rooms[roomIndex].sockets, socket);
+  broadCastDisconnect(socket, index);
+}
 
 function decode(data) {
 	var str = data.toString();
@@ -84,89 +116,128 @@ function encode(type, data) {
 	return Buffer.from(ret);
 }
 
-function broadCast(encodedData) {
-	sockets.forEach(function(socket) {   
+function broadCast(socket, encodedData) {
+	var roomIndex = socketRoomPair[socket.uniqueId];
+	rooms[roomIndex].sockets.forEach(function(socket) {   
     socket.write(encodedData);  
   });  
 }
 
 function handleReceivedData(socket, dataType, data) {
 	switch(dataType) {
+		case "addToRoom":
+			addToRoom(socket);
+			break;
+		case "createNewRoom":
+			createRoom(socket);
+			break;
 		case "requestRandom":
-			sendRandom(data);
+			sendRandom(data, socket);
 			break;
 		case "requestNewRandom":
-			sendRandom(data);
+			sendRandom(data, socket);
 			break;
 		case "generateFloor":
 			generateFloor(data, socket);
 			break;
-		case "deactiveCreate":
-			//sendDeactiveFloor(data, socket);
-			break;
 		case "restartGame":
-			restartGame(data);
+			restartGame(data, socket);
 			break;
 		case "generatePlayer":
 			generateEnemy(data, socket);
 			break;
+		case "getEnemy":
+			getEnemy(data, socket);
+			break;
 		case "playerMove":
 			moveEnemy(data, socket);
 			break;
-		case "useCollectable":
-			sendCollectableIndex(data);
+		case "useGold":
+			sendGoldIndex(data, socket);
 			break;
 		case "playerAnim" :
-			sendEnemyAnim(data);
+			sendEnemyAnim(data, socket);
 			break;
 		case "gameStart":
-			broadCastGameStart(data);
+			broadCastGameStart(data, socket);
 			break;
 		case "addScore":
 			sendAddScore(data, socket);
 			break;
 		case "gameWin":
-			broadCastGameWin(data);
+			broadCastGameWin(data, socket);
 			break;
 		case "playerReady":
-			playerReady(data);
+			playerReady(data, socket);
 			break;
 		case "playerDead":
-			playerDead(data);
+			playerDead(data, socket);
 			break;
 	}
 }
 
-function broadCastDisconnect(index) {
+function broadCastDisconnect(socket, index) {
+	var roomIndex = socketRoomPair[socket.uniqueId];
+	if (roomIndex === undefined)
+		return;
 	var encodedData = encode("playerDisconnect", index.toString());
-	broadCast(encodedData); 
+	broadCast(socket, encodedData); 
 }
 
-function playerReady(data) {
-	readyCount ++;
-	if (readyCount == sockets.length) {
-		restartGame(data);
+function addToRoom(socket) {
+	addSocketToRoom(socket);
+	var encodedData = encode("addToRoom", "");
+	socket.write(encodedData);
+}
+
+function createRoom(socket) {
+	createNewRoom(socket);
+	var encodedData = encode("createNewRoom", "");
+	socket.write(encodedData);
+}
+
+function playerReady(data, socket) {
+	var roomIndex = socketRoomPair[socket.uniqueId];
+	if (roomIndex === undefined)
+		return;
+	rooms[roomIndex].readyCount ++;
+	if (rooms[roomIndex].readyCount >= rooms[roomIndex].sockets.length) {
+		rooms[roomIndex].gameStarted = true;
+		restartGame(data, socket);
 	}
 }
 
-function playerDead(data) {
+function playerDead(data, socket) {
+	var roomIndex = socketRoomPair[socket.uniqueId];
+	if (roomIndex === undefined)
+		return;
 	var encodedData = encode("playerDead", data);
-	broadCast(encodedData); 
+	broadCast(socket, encodedData); 
 }
 
-function broadCastGameStart(data) {
+function broadCastGameStart(data, socket) {
+	var roomIndex = socketRoomPair[socket.uniqueId];
+	if (roomIndex === undefined)
+		return;
 	var encodedData = encode("gameStart", data);
-	broadCast(encodedData); 
+	broadCast(socket, encodedData); 
 }
 
-function broadCastGameWin(data) {
+function broadCastGameWin(data, socket) {
+	var roomIndex = socketRoomPair[socket.uniqueId];
+	if (roomIndex === undefined)
+		return;
 	var encodedData = encode("gameWin", data);
-	broadCast(encodedData); 
+	rooms[roomIndex].gameStarted = false;
+	broadCast(socket, encodedData); 
 }
 
-function sendCollectableIndex(data) {
-	var encodedData = encode("useCollectable", data);
-	broadCast(encodedData); 
+function sendGoldIndex(data, socket) {
+	var roomIndex = socketRoomPair[socket.uniqueId];
+	if (roomIndex === undefined)
+		return;
+	var encodedData = encode("useGold", data);
+	broadCast(socket, encodedData); 
 }
 
 function enableCheat(socket) {
@@ -176,48 +247,50 @@ function enableCheat(socket) {
 }
 
 function sendAddScore(data, socket) {
+	var roomIndex = socketRoomPair[socket.uniqueId];
+	if (roomIndex === undefined)
+		return;
 	var encodedData = encode("addScore", data);
-	sockets.forEach(function(otherSocket) {  
+	rooms[roomIndex].sockets.forEach(function(otherSocket) {  
     if (otherSocket !== socket) {  
       otherSocket.write(encodedData);  
     }  
   }); 
 }
 
-function sendEnemyAnim(data) {
+function sendEnemyAnim(data, socket) {
+	var roomIndex = socketRoomPair[socket.uniqueId];
+	if (roomIndex === undefined)
+		return;
 	var encodedData = encode("enemyAnim", data);
-	broadCast(encodedData); 
+	broadCast(socket, encodedData); 
 }
 
-function sendRandom(data) {
+function sendRandom(data, socket) {
+	var roomIndex = socketRoomPair[socket.uniqueId];
+	if (roomIndex === undefined)
+		return;
 	var index = parseInt(data);
-	var encodedData = encode("receiveRandom", getRandomSeed(index));
-	broadCast(encodedData); 
+	var encodedData = encode("receiveRandom", getRandomSeed(socket, index));
+	broadCast(socket, encodedData); 
 }
 
 function generateFloor(data, socket) {
 	var index = parseInt(data);
-	var encodedData = encode("generateFloor", getRandomSeed(index));
+	var encodedData = encode("generateFloor", getRandomSeed(socket, index));
 
 	socket.write(encodedData); 
 }
 
-function sendDeactiveFloor(data, socket) {
-	var newIndex = parseInt(data);
-	var encodedData = encode("deactiveCreate", getRandomSeed(newIndex));
-	sockets.forEach(function(otherSocket) {  
-    if (otherSocket !== socket) {  
-        otherSocket.write(encodedData);  
-    }  
-  }); 
-}
-
-function restartGame(data) {
-	clearSeed();
-	readyCount = 0;
-	var encodedData = encode("receiveRandom", getRandomSeed(0));
-	broadCast(encodedData);
-	sockets.forEach(function(socket, index) {   
+function restartGame(data, socket) {
+	var roomIndex = socketRoomPair[socket.uniqueId];
+	if (roomIndex === undefined)
+		return;
+	clearSeed(roomIndex);
+	rooms[roomIndex].readyCount = 0;
+	var encodedData = encode("receiveRandom", getRandomSeed(socket, 0));
+	broadCast(socket, encodedData);
+	rooms[roomIndex].sockets.forEach(function(socket, index) {   
 		var encodedData = encode("restartGame", index.toString());
 		if (index != 0) {
 			enableCheat(socket);
@@ -228,38 +301,57 @@ function restartGame(data) {
 
 function generateEnemy(data, socket) {
 	var socketIndex;
-	sockets.forEach(function(otherSocket, index) {   
+	var roomIndex = socketRoomPair[socket.uniqueId];
+	if (roomIndex === undefined)
+		return;
+	rooms[roomIndex].sockets.forEach(function(otherSocket, index) {   
     if (otherSocket === socket) {  
       socketIndex = index; 
     }
   }); 
-	sockets.forEach(function(otherSocket) {   
+  socket.playerName = data;
+	rooms[roomIndex].sockets.forEach(function(otherSocket) {   
     if (otherSocket !== socket) {  
-      otherSocket.write(encode("generateEnemy", socketIndex.toString()));  
+      otherSocket.write(encode("generateEnemy", socketIndex.toString() + "," + data));  
     }  else {
-    	otherSocket.write(encode("generatePlayer", socketIndex.toString()));  
+    	otherSocket.write(encode("generatePlayer", socketIndex.toString()+ "," + data));  
     }
   });  
 }
 
+function getEnemy(data, socket) {
+	var roomIndex = socketRoomPair[socket.uniqueId];
+	if (roomIndex === undefined)
+		return;
+	var enemyIndex = parseInt(data);
+	var enemyName = rooms[roomIndex].sockets[enemyIndex].playerName;
+	socket.write(encode("generateEnemy", data + "," + enemyName));  
+}
+
 function moveEnemy(data, socket) {
-	sockets.forEach(function(otherSocket, index) {   
+	var roomIndex = socketRoomPair[socket.uniqueId];
+	if (roomIndex === undefined)
+		return;
+	rooms[roomIndex].sockets.forEach(function(otherSocket, index) {   
     if (otherSocket !== socket) {  
       otherSocket.write(encode("enemyMove", data));  
     }
   }); 
 }
 
-function clearSeed() {
-	randomSeeds = [];
+function clearSeed(roomIndex) {
+	rooms[roomIndex].randomSeeds = [];
 }
 
-function getRandomSeed(index) {
-	while(index >= randomSeeds.length) {
+function getRandomSeed(socket, index) {
+	var roomIndex = socketRoomPair[socket.uniqueId];
+	if (roomIndex === undefined)
+		return;
+	while(index >= rooms[roomIndex].randomSeeds.length) {
 		var date = new Date();
-		randomSeeds.push(date.getTime());
+		rooms[roomIndex].randomSeeds.push(date.getTime());
 	}
 
-	var randomStr = randomSeeds[index].toString();
+	var randomStr = rooms[roomIndex].randomSeeds[index].toString();
 	return randomStr.slice(randomStr.length - 6, randomStr.length - 1);
 }
